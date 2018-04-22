c43fec1c9df551366df09d59058b29f338c19291
Jwing28 <jonathan.wing.lee@gmail.com>
2018-04-21 22:25:28 -0500

    updated user schema

--- server/schema/users.js
+++ server/schema/users.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   favorites: Array
 });
 
