3ccf908cb4af00ab44ee039aee191ebb3bc3e6bc
Jwing28 <jonathan.wing.lee@gmail.com>
2018-04-24 18:09:51 -0500

    wrote logic for retrieving recipes, to be implemented in results component

--- react-ui/src/users/profile.js
+++ react-ui/src/users/profile.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
-import Card from '../components/card';
 import { withRouter } from 'react-router-dom';
+import Grid from '../components/grid';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { user:{}, ingredients:[]};
+  }
+
+  componentDidMount() {
+    //go get the api data to render
+    let apiKey = 'c25afe65342f2138c001fcb789db1059';
+    //http://food2fork.com/api/search
+    //q=comma separated strings of ingredients
+    //sort=r most popular recipes with these ingredients
+    //max 30 results returned
+    let url = `http://cors-proxy.htmldriven.com/?url=http://food2fork.com/api/search?key=c25afe65342f2138c001fcb789db1059&sort=r&q=shredded%20chicken`;
+    //let ingredients = this.state.ingredients.map((ingredient) => `${}`)
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(recipes => {
+        console.log('data??', JSON.parse(recipes.body))
+      })
+      .catch(error => {
+        console.log(`Error retreiving recipe data ${error}`)
+      });
+  }
+
   render() {
     //this.props.location.state.user.username or favorites
     return(
       <div>
         Welcome {this.props.location.state.user.username} !
-        <Card />
+
       </div>
     );
   }
