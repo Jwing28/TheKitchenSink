cde445c779ecec1822179bde91806dfc9fdb0eb2
Jwing28 <jonathan.wing.lee@gmail.com>
2018-04-25 19:10:31 -0500

    improved card styling and layout

--- react-ui/src/components/card.js
+++ react-ui/src/components/card.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Button, Col, Thumbnail } from 'react-bootstrap';
+import { Button, Col, Panel } from 'react-bootstrap';
 import './styles/card.css';
 
 const Card = ({ recipeName, link, image }) => (
   <Col xs={6} md={4} className="card-container">
-    <Thumbnail src={image} alt="Food Image" className="card-thumbnail-container">
-      <h3 className="card-recipe-name">{recipeName || "Recipe Name"}</h3>
-      <p className="card-actions">
-        <Button bsStyle="info">Info</Button>
-        <Button bsStyle="success">Save</Button>
-      </p>
-    </Thumbnail>
+    <Panel bsStyle="primary">
+      <Panel.Heading>
+        <Panel.Title componentClass="h3" className="card-recipe-name">{recipeName || "Recipe Name"}</Panel.Title>
+      </Panel.Heading>
+      <img src={image} alt="Food Image" className="card-thumbnail-container" height={200} width={200} />
+        <p className="card-actions">
+          <Button bsStyle="info">Info</Button>
+          <Button bsStyle="success">Save</Button>
+        </p>
+    </Panel>
   </Col>
 );
 
