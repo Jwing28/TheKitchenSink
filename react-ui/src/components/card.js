cd3423b6bc50d9251b3da056622408c2a5c6d2db
Jwing28 <jonathan.wing.lee@gmail.com>
2018-04-25 19:19:32 -0500

    fixed responsiveness issue with cards

--- react-ui/src/components/card.js
+++ react-ui/src/components/card.js
@@ -3,12 +3,12 @@ import { Button, Col, Panel } from 'react-bootstrap';
 import './styles/card.css';
 
 const Card = ({ recipeName, link, image }) => (
-  <Col xs={6} md={4} className="card-container">
+  <Col xs={12} sm={6} md={4} lg={3} className="card-container">
     <Panel bsStyle="primary">
       <Panel.Heading>
         <Panel.Title componentClass="h3" className="card-recipe-name">{recipeName || "Recipe Name"}</Panel.Title>
       </Panel.Heading>
-      <img src={image} alt="Food Image" className="card-thumbnail-container" height={200} width={200} />
+      <img src={image} alt="Food Image" className="card-thumbnail-container"  />
         <p className="card-actions">
           <Button bsStyle="info">Info</Button>
           <Button bsStyle="success">Save</Button>
