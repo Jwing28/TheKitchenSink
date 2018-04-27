fd806867b5b73a582983286d103812a6ebaa59ea
Jwing28 <jonathan.wing.lee@gmail.com>
2018-04-27 01:20:54 -0500

    added header/footer to landing component along with styling

--- react-ui/src/home/Landing.js
+++ react-ui/src/home/Landing.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import './styles/Landing.css';
 
 const Landing = () => (
-  <div>
-    <div><Link to='/login'>Login</Link></div>
-    <div><Link to='/signup'>Signup</Link></div>
+  <div className="Landing-container">
+    <Header />
+      <div className="Landing-content">
+        <h1>
+          TheKitchenSink
+        </h1>
+        <div className="Landing-actions">
+          <Link to='/login'><button className="Landing-buttons">Login</button></Link>
+          <Link to='/signup'><button className="Landing-buttons">Sign Up</button></Link>
+        </div>
+      </div>
+    <Footer />
   </div>
 );
 export default Landing;
