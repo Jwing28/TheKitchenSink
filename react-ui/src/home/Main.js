1ce5f6b4892318c8c7027b7826960f5fa24764d7
Jwing28 <jonathan.wing.lee@gmail.com>
2018-05-07 23:16:22 -0500

    installed enzyme and jest for testing and fixed provided jest sample test for app component

--- react-ui/src/home/Main.js
+++ react-ui/src/home/Main.js
@@ -6,20 +6,23 @@ import Landing from './Landing';
 import Profile from '../users/profile';
 import Recipes from '../users/Recipes';
 import FourOhFour from '../components/404';
+import { BrowserRouter } from 'react-router-dom';
 
 
 //we want user to go to login OR sign up, not both = switch
 class Main extends Component {
   render() {
     return(
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/login" component={Login}/>
-        <Route path="/signup" component={Signup}/>
-        <Route path="/profile" component={Profile}/>
-        <Route path="/recipes" component={Recipes}/>
-        <Route component={FourOhFour}/>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/login" component={Login}/>
+          <Route path="/signup" component={Signup}/>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/recipes" component={Recipes}/>
+          <Route component={FourOhFour}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
